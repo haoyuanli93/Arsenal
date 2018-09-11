84f24aa2f363830c56e028ed75a7fb6122613fe1
Haoyuan Li <hyli16@stanford.edu>
2018-09-11 13:19:48 -0700

    time stamp

--- arsenal/__init__.py
+++ arsenal/__init__.py
@@ -1,3 +1,4 @@
 import arsenal.stats
 import arsenal.radial
 import arsenal.fitting
+import arsenal.util
---
+++ arsenal/util.py
@@ -0,0 +1,11 @@
+import time
+import datetime
+
+
+def time_stamp():
+    """
+    Get a time stamp
+    :return: A time stamp of the form '%Y_%m_%d_%H_%M_%S'
+    """
+    stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d_%H_%M_%S')
+    return stamp
