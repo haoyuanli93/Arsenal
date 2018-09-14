7d51b1b6692c39c3a86130d1f1fb4c9c44b04159
Haoyuan Li <hyli16@stanford.edu>
2018-09-14 14:44:09 -0700

    minor

--- arsenal/lcls.py
+++ arsenal/lcls.py
@@ -1,4 +1,5 @@
 import psana
+import numpy as np
 
 
 ###########################################################################################################
@@ -6,8 +7,7 @@ import psana
 ###########################################################################################################
 
 
-def setup_exp(exp_name, run_num,
-              det_name,
+def setup_exp(exp_name, run_num, det_name,
               mask_calib_on=True,
               mask_status_on=True,
               mask_edges_on=True,
@@ -15,40 +15,65 @@ def setup_exp(exp_name, run_num,
               mask_unbond_on=True,
               mask_unbondnrs_on=True):
     """
-    
-    :param exp_name: 
-    :param run_num: 
+    This function is used to initialize the detector object. The return is a dictionary.
+    The name of the dictionary is suggestive.
+
+    :param exp_name: The experiment name
+    :param run_num: The run number to inspect
+    :param det_name: The detector name
     :param mask_calib_on: 
     :param mask_status_on: 
     :param mask_edges_on: 
     :param mask_central_on: 
     :param mask_unbond_on: 
     :param mask_unbondnrs_on: 
-    :return: 
+    :return: detector object, run object, times object, 2d pattern shape, pattern stack shape, mask stack, 2d mask
     """
 
+    # Initialize the datasource
     ds = psana.DataSource('exp={}:run{}:idx'.format(exp_name, run_num))
     run = ds.runs().next()
     times = run.times()
     env = ds.env()
+    evt = run.event(times[0])
 
+    # Get the detector
     det = psana.Detector(det_name, env)
-    et = psana.EventTime(int(ts[0]), fid[0])
-    evt = run.event(et)
+
+    # Generate a holder for some experiment information
+    holder = {}
+
+    # Get pattern shapes
     example = det.image(evt)
-    shape = example.shape
+    holder.update({'Example 2D': example,
+                   '2D pattern shape': example.shape})
+
+    # Get pattern stack shape
+    example = det.calib(evt)
+    holder.update({'Example stack': example,
+                   'Pattern stack shape': example.shape})
 
     # get mask and save mask
     mask_stack = det.mask(evt, calib=mask_calib_on, status=mask_status_on,
                           edges=mask_edges_on, central=mask_central_on,
                           unbond=mask_unbond_on, unbondnbrs=mask_unbondnrs_on)
     mask_2d = det.image(evt, mask_stack)
+    holder.update({'Mask 2D': mask_2d,
+                   'Mask stack': mask_stack})
 
-    return
+    return det, run, times, holder
 
 
-# Get a sample
+# Get a calibrated sample
 def get_pattern_stack(detector, exp_run, event_id):
+    """
+    Get a pattern stack from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param event_id: The event it
+    :return: The pattern stack
+    """
     times = exp_run.times()
     evt = exp_run.event(times[event_id])
     pattern_stack = detector.calib(evt)
@@ -56,7 +81,152 @@ def get_pattern_stack(detector, exp_run, event_id):
 
 
 def get_pattern_2d(detector, exp_run, event_id):
+    """
+    Get a pattern from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param event_id: The event it
+    :return: The pattern
+    """
     times = exp_run.times()
     evt = exp_run.event(times[event_id])
     pattern_2d = detector.image(evt)
     return pattern_2d
+
+
+# Get a photon number sample
+def get_photon_stack(detector, exp_run, event_id):
+    """
+    Get a photon stack from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param event_id: The event it
+    :return: The pattern stack
+    """
+    times = exp_run.times()
+    evt = exp_run.event(times[event_id])
+    pattern_stack = detector.photons(evt)
+    return pattern_stack
+
+
+def get_photon_2d(detector, exp_run, event_id):
+    """
+    Get a photon number pattern from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param event_id: The event it
+    :return: The pattern
+    """
+    times = exp_run.times()
+    evt = exp_run.event(times[event_id])
+    pattern_stack = detector.photons(evt=evt)
+    pattern_2d = detector.image(evt=evt, nda_in=pattern_stack)
+    return pattern_2d
+
+
+# Get a calibrated sample
+def get_pattern_stack_fast(detector, exp_run, exp_times, event_id):
+    """
+    Get a pattern stack from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param exp_times: The time object
+    :param event_id: The event it
+    :return: The pattern stack
+    """
+    evt = exp_run.event(exp_times[event_id])
+    pattern_stack = detector.calib(evt)
+    return pattern_stack
+
+
+def get_pattern_2d_fast(detector, exp_run, exp_times, event_id):
+    """
+    Get a pattern from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param exp_times: The time object
+    :param event_id: The event it
+    :return: The pattern
+    """
+    evt = exp_run.event(exp_times[event_id])
+    pattern_2d = detector.image(evt)
+    return pattern_2d
+
+
+# Get a photon number sample
+def get_photon_stack_fast(detector, exp_run, exp_times, event_id):
+    """
+    Get a photon stack from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param exp_times: The time object
+    :param event_id: The event it
+    :return: The pattern stack
+    """
+    evt = exp_run.event(exp_times[event_id])
+    pattern_stack = detector.photons(evt)
+    return pattern_stack
+
+
+def get_photon_2d_fast(detector, exp_run, exp_times, event_id):
+    """
+    Get a photon number pattern from the detector from the specific run for the specified event id
+
+    :param detector: The detector object
+    :param exp_run: The run object
+    :param exp_times: The time object
+    :param event_id: The event it
+    :return: The pattern
+    """
+    evt = exp_run.event(exp_times[event_id])
+    pattern_stack = detector.photons(evt=evt)
+    pattern_2d = detector.image(evt=evt, nda_in=pattern_stack)
+    return pattern_2d
+
+
+###########################################################################################################
+# Psocake
+###########################################################################################################
+def cast_numpy_to_txt(arr, output_file):
+    """
+    In psocake, in spi mode, current, the generate average pattern function only produce
+    txt files for max. However, the mean patterns are also desirable for distance calibration.
+    Therefore, this function can be used to cast the stack pattern, numpy array to a text file.
+
+    :param arr: The numpy array to cast.
+    :param output_file: The output file that is compatible with calibman
+    :return: None
+    """
+    shape = arr.shape
+    arr = arr.reshape([shape[0] * shape[1], shape[2]])
+
+    np.savetxt(fname=output_file, X=arr, delimiter=' ', fmt='%.18e', newline='\n', )
+
+
+def cast_txt_to_numpy(iuput_file):
+    """
+    The inverse function of the previous function.
+    This one read the txt file and cast that into a numpy array
+
+    :param iuput_file: The txt file to process.
+    :return: The array.
+    """
+    # Load the txt file
+    with open(iuput_file, 'r') as tmpfile:
+        lines = tmpfile.readlines()
+
+        # Restore the numpy array
+        holder = []
+        for line in lines:
+            holder.append([float(x) for x in line.split(' ')])
+
+        # Construct the numpy array
+        holder = np.array(holder)
+        
+    return holder
