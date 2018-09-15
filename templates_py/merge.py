1d296d698d9bc3a2d96292f7541b7752c984e226
Haoyuan Li <hyli16@stanford.edu>
2018-09-15 04:17:05 -0700

    minor

--- templates_py/merge.py
+++ templates_py/merge.py
@@ -1,45 +1,10 @@
 from numba import jit
 import sys
 import numpy as np
-import mpi4py.MPI as MPI
 import time
 sys.path.append('/reg/neh/home5/haoyuan/Documents/my_repos/Arsenal')
 import arsenal
-comm = MPI.COMM_WORLD
-comm_rank = comm.Get_rank()
-comm_size = comm.Get_size()
 
-
-@jit
-def make_rot_quat(quaternion):
-    rot = np.zeros([3, 3])
-
-    q0 = quaternion[0]
-    q1 = quaternion[1]
-    q2 = quaternion[2]
-    q3 = quaternion[3]
-
-    q01 = q0 * q1
-    q02 = q0 * q2
-    q03 = q0 * q3
-    q11 = q1 * q1
-    q12 = q1 * q2
-    q13 = q1 * q3
-    q22 = q2 * q2
-    q23 = q2 * q3
-    q33 = q3 * q3
-
-    rot[0, 0] = (1. - 2. * (q22 + q33))
-    rot[0, 1] = 2. * (q12 + q03)
-    rot[0, 2] = 2. * (q13 - q02)
-    rot[1, 0] = 2. * (q12 - q03)
-    rot[1, 1] = (1. - 2. * (q11 + q33))
-    rot[1, 2] = 2. * (q01 + q23)
-    rot[2, 0] = 2. * (q02 + q13)
-    rot[2, 1] = 2. * (q23 - q01)
-    rot[2, 2] = (1. - 2. * (q11 + q22))
-
-    return rot.T
 
 
 @jit
