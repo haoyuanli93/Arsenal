a6197588092a89688300232f59e82bea8d0a2406
Haoyuan Li <hyli16@stanford.edu>
2018-09-22 15:51:18 -0700

    minor

--- arsenal/HoloviewsUtil.py
+++ arsenal/HoloviewsUtil.py
@@ -6,58 +6,57 @@ import holoviews as hv
 # Assemble and show the patterns
 ########################################################################################################
 def assemble_patterns_image(data_holder, data_shape, row_num, col_num, index, value_range, height, width):
-        """
-        After the program has obtained the index of the patterns in the selected region,
-        this function randomly choose several of the patterns to show in a grid-space.
+    """
+    After the program has obtained the index of the patterns in the selected region,
+    this function randomly choose several of the patterns to show in a grid-space.
 
-        :param data_holder: The holder containing all the data shown in the diagram
-        :param row_num: The row number of the grid space
-        :param col_num: The column number of the grid space
-        :param index: The index of all the data in the selected region
-        :param value_range: The range of values to show a numpy array as RGB image.
-        :param data_shape: The pattern shape
-        :param height: The height of the samples in the sample panel
-        :param width: The width of the samples in the sample panel.
-        :return: hv.GridSpace
-        """
-        index = np.array(index)
-        index_num = index.shape[0]
+    :param data_holder: The holder containing all the data shown in the diagram
+    :param row_num: The row number of the grid space
+    :param col_num: The column number of the grid space
+    :param index: The index of all the data in the selected region
+    :param value_range: The range of values to show a numpy array as RGB image.
+    :param data_shape: The pattern shape
+    :param height: The height of the samples in the sample panel
+    :param width: The width of the samples in the sample panel.
+    :return: hv.GridSpace
+    """
+    index = np.array(index)
+    index_num = index.shape[0]
 
-        if index_num >= row_num * col_num:
+    if index_num >= row_num * col_num:
 
-            # Randomly choose some index
-            np.random.shuffle(index)
-            # idxes: indexes for the chosen patterns
-            idxes = index[:row_num * col_num]
-            idxes = idxes.reshape((row_num, col_num))
+        # Randomly choose some index
+        np.random.shuffle(index)
+        # idxes: indexes for the chosen patterns
+        idxes = index[:row_num * col_num]
+        idxes = idxes.reshape((row_num, col_num))
 
-            # Create a holder
-            image_holder = {}
-            for x in range(row_num):
-                for y in range(col_num):
-                    tmp_image = hv.Image(data_holder[idxes[x, y]]).options(height=height,
-                                                                           width=width).redim.range(z=(value_range[0],
-                                                                                                       value_range[1]))
+        # Create a holder
+        image_holder = {}
+        for x in range(row_num):
+            for y in range(col_num):
+                tmp_image = hv.Image(data_holder[idxes[x, y]]).options(height=height,
+                                                                       width=width).redim.range(z=(value_range[0],
+                                                                                                   value_range[1]))
 
-                    image_holder.update({(x, y): tmp_image})
+                image_holder.update({(x, y): tmp_image})
 
-        else:
-            # When we do not have so many patterns, first show all the patterns available
-            index_list = [(x, y) for x in range(row_num) for y in range(col_num)]
-            image_holder = {}
-            for l in range(index_num):
-                tmp_image = hv.Image(data_holder[index[l]]).options(height=height,
-                                                                    width=width
-                                                                    ).redim.range(z=(value_range[0],
-                                                                                     value_range[1]))
-                image_holder.update({index_list[l]: tmp_image})
+    else:
+        # When we do not have so many patterns, first show all the patterns available
+        index_list = [(x, y) for x in range(row_num) for y in range(col_num)]
+        image_holder = {}
+        for l in range(index_num):
+            tmp_image = hv.Image(data_holder[index[l]]).options(height=height,
+                                                                width=width
+                                                                ).redim.range(z=(value_range[0],
+                                                                                 value_range[1]))
+            image_holder.update({index_list[l]: tmp_image})
 
-            # Use blank image to fill in the other spaces
-            image_holder.update({index_list[l]: hv.Image(np.zeros(data_shape,
-                                                                  dtype=np.float64))
-                                 for l in range(index_num, row_num * col_num)})
+        # Use blank image to fill in the other spaces
+        image_holder.update({index_list[l]: hv.Image(np.zeros(data_shape, dtype=np.float64))
+                             for l in range(index_num, row_num * col_num)})
 
-        return hv.GridSpace(image_holder)
+    return hv.GridSpace(image_holder)
 
 
 def assemble_patterns_curve(y_data_array, y_range, x_data, x_range, row_num, col_num, index):
