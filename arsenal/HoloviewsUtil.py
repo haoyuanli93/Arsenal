97654f0e271a53f68fdde096b3a47e77cf7696c6
Haoyuan Li <hyli16@stanford.edu>
2018-09-23 14:30:25 -0700

    minor

--- arsenal/HoloviewsUtil.py
+++ arsenal/HoloviewsUtil.py
@@ -62,10 +62,10 @@ def assemble_patterns_image(data_holder, data_shape, row_num, col_num, index, va
                                                                                          cmap='jet')
                              for l in range(index_num, row_num * col_num)})
 
-    return hv.GridSpace(image_holder)
+    return hv.GridSpace(image_holder).options(shared_xaxis=False, shared_yaxis=False)
 
 
-def assemble_patterns_curve(y_data_array, y_range, x_data, x_range, row_num, col_num, index):
+def assemble_patterns_curve(y_data_array, x_data, row_num, col_num, index, height, width):
     """
     After the program has obtained the index of the patterns in the selected region,
     this function randomly choose several of the patterns to show in a grid-space.
@@ -74,12 +74,12 @@ def assemble_patterns_curve(y_data_array, y_range, x_data, x_range, row_num, col
     assume that all curves share the same x-axis.
 
     :param y_data_array: 2D numpy array of the shape [n, curve_length] where n is the pattern number in total.
-    :param y_range: The y range of the pattern.
     :param x_data: The x coordinate of the curve.
-    :param x_range: The x range of the pattern.
     :param row_num: The row number of the grid space
     :param col_num: The column number of the grid space
     :param index: The index of all the data in the selected region
+    :param height: The height of the samples in the sample panel
+    :param width: The width of the samples in the sample panel.
     :return: hv.GridSpace
     """
     index = np.array(index)
@@ -88,172 +88,61 @@ def assemble_patterns_curve(y_data_array, y_range, x_data, x_range, row_num, col
     # Get the span of x
     x_span = (x_data.max() - x_data.min())
 
-    if y_range == 'auto' and x_range == 'auto':
-        if index_num >= row_num * col_num:
+    if index_num >= row_num * col_num:
 
-            # Extract some samples from all the selected ones.
-            np.random.shuffle(index)
-            sampled_index = index[:row_num * col_num]
-            sampled_index = sampled_index.reshape((row_num, col_num))
+        # Extract some samples from all the selected ones.
+        np.random.shuffle(index)
+        sampled_index = index[:row_num * col_num]
+        sampled_index = sampled_index.reshape((row_num, col_num))
 
-            # Assemble the patterns
-            image_holder = {}
-            for x in range(row_num):
-                for y in range(col_num):
-                    # Get the span of the y values
-                    y_data = y_data_array[sampled_index[x, y]]
-                    y_span = y_data.max() - y_data.min()
-
-                    image_holder.update({(x, y): hv.Curve((x_data, y_data)).redim.range(
-                        x=(x_data.min() - x_span * 0.05,
-                           x_data.max() + x_span * 0.05),
-                        y=(y_data.min() - y_span * 0.05,
-                           y_data.max() + y_span * 0.05))})
-        else:
-            # When we do not have so many patterns, first layout
-            # all the patterns available and then fill the other
-            # positions with patterns of zeros.
-            index_list = [(x, y) for x in range(row_num) for y in range(col_num)]
-
-            # Assemble the True patterns
-            image_holder = {}
-            for l in range(index_num):
+        # Assemble the patterns
+        image_holder = {}
+        for x in range(row_num):
+            for y in range(col_num):
                 # Get the span of the y values
-                y_data = y_data_array[index[l]]
+                y_data = y_data_array[sampled_index[x, y]]
                 y_span = y_data.max() - y_data.min()
 
-                image_holder.update({index_list[l]: hv.Curve((x_data, y_data)).redim.range(
-                    x=(x_data.min() - x_span * 0.05,
-                       x_data.max() + x_span * 0.05),
-                    y=(y_data.min() - y_span * 0.05,
-                       y_data.max() + y_span * 0.05))})
+                image_holder.update({(x, y): hv.Curve((x_data,
+                                                       y_data
+                                                       )).options(width=width,
+                                                                  height=height
+                                                                  ).redim.range(x=(x_data.min() - x_span * 0.05,
+                                                                                   x_data.max() + x_span * 0.05),
+                                                                                y=(y_data.min() - y_span * 0.05,
+                                                                                   y_data.max() + y_span * 0.05))})
+    else:
+        # When we do not have so many patterns, first layout
+        # all the patterns available and then fill the other
+        # positions with patterns of zeros.
+        index_list = [(x, y) for x in range(row_num) for y in range(col_num)]
 
-            # Assemble the psudo-image
-            image_holder.update({index_list[l]: hv.Curve((x_data, np.zeros_like(x_data))).redim.range(
-                x=(x_data.min() - x_span * 0.05, x_data.max() + x_span * 0.05), y=(0, 1))
-                for l in range(index_num, row_num * col_num)})
+        # Assemble the True patterns
+        image_holder = {}
+        for l in range(index_num):
+            # Get the span of the y values
+            y_data = y_data_array[index[l]]
+            y_span = y_data.max() - y_data.min()
 
-    elif y_range == 'auto' and x_range != 'auto':  # If y_range is auto while x is specified
+            image_holder.update({index_list[l]: hv.Curve((x_data,
+                                                          y_data)
+                                                         ).options(width=width,
+                                                                   height=height
+                                                                   ).redim.range(x=(x_data.min() - x_span * 0.05,
+                                                                                    x_data.max() + x_span * 0.05),
+                                                                                 y=(y_data.min() - y_span * 0.05,
+                                                                                    y_data.max() + y_span * 0.05))})
 
-        if index_num >= row_num * col_num:
+        # Assemble the psudo-image
+        image_holder.update({index_list[l]: hv.Curve((x_data,
+                                                      np.zeros_like(x_data))
+                                                     ).options(width=width,
+                                                               height=height
+                                                               ).redim.range(x=(x_data.min()
+                                                                                - x_span * 0.05,
+                                                                                x_data.max()
+                                                                                + x_span * 0.05),
+                                                                             y=(-0.1, 1))
+                             for l in range(index_num, row_num * col_num)})
 
-            # Extract some samples from all the selected ones.
-            np.random.shuffle(index)
-            sampled_index = index[:row_num * col_num]
-            sampled_index = sampled_index.reshape((row_num, col_num))
-
-            # Assemble the patterns
-            image_holder = {}
-            for x in range(row_num):
-                for y in range(col_num):
-                    # Get the span of the y values
-                    y_data = y_data_array[sampled_index[x, y]]
-                    y_span = y_data.max() - y_data.min()
-
-                    image_holder.update({(x, y): hv.Curve((x_data, y_data)).redim.range(
-                        x=(x_range[0], x_range[1]),
-                        y=(y_data.min() - y_span * 0.05,
-                           y_data.max() + y_span * 0.05))})
-        else:
-            # When we do not have so many patterns, first layout
-            # all the patterns available and then fill the other
-            # positions with patterns of zeros.
-            index_list = [(x, y) for x in range(row_num) for y in range(col_num)]
-
-            # Assemble the True patterns
-            image_holder = {}
-            for l in range(index_num):
-                # Get the span of the y values
-                y_data = y_data_array[index[l]]
-                y_span = y_data.max() - y_data.min()
-
-                image_holder.update({index_list[l]: hv.Curve((x_data, y_data)).redim.range(
-                    x=(x_range[0], x_range[1]),
-                    y=(y_data.min() - y_span * 0.05,
-                       y_data.max() + y_span * 0.05))})
-
-            # Assemble the psudo-image
-            image_holder.update({index_list[l]: hv.Curve((x_data, np.zeros_like(x_data))).redim.range(
-                x=(x_range[0], x_range[1]), y=(0, 1))
-                for l in range(index_num, row_num * col_num)})
-
-    elif y_range != 'auto' and x_range == 'auto':
-
-        if index_num >= row_num * col_num:
-
-            # Extract some samples from all the selected ones.
-            np.random.shuffle(index)
-            sampled_index = index[:row_num * col_num]
-            sampled_index = sampled_index.reshape((row_num, col_num))
-
-            # Assemble the patterns
-            image_holder = {}
-            for x in range(row_num):
-                for y in range(col_num):
-                    # Get the span of the y values
-                    y_data = y_data_array[sampled_index[x, y]]
-
-                    image_holder.update({(x, y): hv.Curve((x_data, y_data)).redim.range(
-                        x=(x_data.min() - x_span * 0.05,
-                           x_data.max() + x_span * 0.05),
-                        y=(y_range[0], y_range[1]))})
-        else:
-            # When we do not have so many patterns, first layout
-            # all the patterns available and then fill the other
-            # positions with patterns of zeros.
-            index_list = [(x, y) for x in range(row_num) for y in range(col_num)]
-
-            # Assemble the True patterns
-            image_holder = {}
-            for l in range(index_num):
-                # Get the span of the y values
-                y_data = y_data_array[index[l]]
-
-                image_holder.update({index_list[l]: hv.Curve((x_data, y_data)).redim.range(
-                    x=(x_data.min() - x_span * 0.05,
-                       x_data.max() + x_span * 0.05),
-                    y=(y_range[0], y_range[1]))})
-
-            # Assemble the psudo-image
-            image_holder.update({index_list[l]: hv.Curve((x_data, np.zeros_like(x_data))).redim.range(
-                x=(x_data.min() - x_span * 0.05, x_data.max() + x_span * 0.05), y=(y_range[0], y_range[1]))
-                for l in range(index_num, row_num * col_num)})
-
-    else:  # If neither x_range or y_range is specified
-        if index_num >= row_num * col_num:
-
-            # Extract some samples from all the selected ones.
-            np.random.shuffle(index)
-            sampled_index = index[:row_num * col_num]
-            sampled_index = sampled_index.reshape((row_num, col_num))
-
-            # Assemble the patterns
-            image_holder = {}
-            for x in range(row_num):
-                for y in range(col_num):
-                    # Get the span of the y values
-                    y_data = y_data_array[sampled_index[x, y]]
-
-                    image_holder.update({(x, y): hv.Curve((x_data, y_data)).redim.range(
-                        x=(x_range[0], x_range[1]), y=(y_range[0], y_range[1]))})
-        else:
-            # When we do not have so many patterns, first layout
-            # all the patterns available and then fill the other
-            # positions with patterns of zeros.
-            index_list = [(x, y) for x in range(row_num) for y in range(col_num)]
-
-            # Assemble the True patterns
-            image_holder = {}
-            for l in range(index_num):
-                # Get the span of the y values
-                y_data = y_data_array[index[l]]
-
-                image_holder.update({index_list[l]: hv.Curve((x_data, y_data)).redim.range(
-                    x=(x_range[0], x_range[1]), y=(y_range[0], y_range[1]))})
-
-            # Assemble the psudo-image
-            image_holder.update({index_list[l]: hv.Curve((x_data, np.zeros_like(x_data))).redim.range(
-                x=(x_range[0], x_range[1]), y=(y_range[0], y_range[1]))
-                for l in range(index_num, row_num * col_num)})
-
-    return hv.GridSpace(image_holder)
+    return hv.GridSpace(image_holder).options(shared_xaxis=False, shared_yaxis=False)
