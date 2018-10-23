dc9b0bd283d9887681ee22a0ad7664f99cb11ae0
Haoyuan Li <hyli16@stanford.edu>
2018-10-22 19:30:43 -0700

    minor

--- arsenal/PsanaUtil.py
+++ arsenal/PsanaUtil.py
@@ -1,4 +1,8 @@
+import numpy as np
 import psana
+
+import arsenal.geometry as ag
+import arsenal.physics as ap
 
 """
 This module focuses on applications in LCLS that requires psana
@@ -186,3 +190,94 @@ def get_photon_2d_fast(detector, exp_run, exp_times, event_id, adu_per_photon):
     pattern_stack = detector.photons(evt=evt, adu_per_photon=adu_per_photon)
     pattern_2d = detector.image(evt=evt, nda_in=pattern_stack)
     return pattern_2d
+
+
+def get_pixel_info(detector, run_num):
+    """
+    Get the pixel position of the detector in the unit of meter.
+
+    :param detector: The pnccd detector object
+    :param run_num:
+    :return: A dictionary containing useful info.
+    """
+
+    # Get detector pixel information
+    tmp_pos = detector.coords_xyz(par=run_num)
+    pixel_areas_stack = detector.areas(par=run_num)
+    pixel_areas_stack *= 1e-12  # convert the unit to meter
+
+    # Get shape and pixel number
+    stack_shape = detector.pedestals(par=run_num).shape
+    pixel_num = int(np.prod(stack_shape))
+    pixel_areas_1d = np.reshape(pixel_areas_stack, (pixel_num,))
+
+    # Extrach pixel position info
+    pixel_position_1d = np.zeros((pixel_num, 3), dtype=np.float64)
+    for l in range(3):
+        pixel_position_1d[:, l] = np.reshape(tmp_pos[l], pixel_num)
+
+    # Convert to meter
+    pixel_position_1d *= 1e-6
+
+    # Get the pixel distance and direction
+    pixel_distances_1d = np.sqrt(np.sum(np.square(pixel_position_1d), axis=-1))
+    pixel_directions_1d = np.divide(pixel_position_1d, pixel_distances_1d[:, np.newaxis])
+
+    # Get the stack shape
+    pixel_position_stack = np.reshape(pixel_position_1d, stack_shape + (3,))
+    pixel_directions_stack = np.reshape(pixel_directions_1d, stack_shape + (3,))
+    pixel_distance_stack = np.reshape(pixel_distances_1d, stack_shape)
+
+    # Assemble the results in to dictionary
+    info = {'pixel num': pixel_num,
+            'stack shape': stack_shape,
+
+            'pixel position 1d': pixel_position_1d,
+            'pixel distance 1d': pixel_distances_1d,
+            'pixel area 1d': pixel_areas_1d,
+            'pixel direction 1d': pixel_directions_1d,
+
+            'pixel position stack': pixel_position_stack,
+            'pixel distance stack': pixel_distance_stack,
+            'pixel area stack': pixel_areas_stack,
+            'pixel direction stack': pixel_directions_stack}
+
+    return info
+
+
+def get_detector_polarization_correction(detector, run_num, polarization):
+    """
+    Get the detector polarization correction based on the detector object in psana.
+
+    :param detector: psana detector object.
+    :param polarization: The polarization of the beam.
+    :param run_num: The run number of the detector object
+    :return:
+    """
+    pixel_info = get_pixel_info(detector=detector, run_num=run_num)
+
+    polarization_correction = ap.get_intensity_polarization_effect(pixel_position_m=pixel_info['pixel position 1d'],
+                                                                   reference_position_m=np.zeros(3, dtype=np.float64),
+                                                                   polarization=polarization)
+    return polarization_correction
+
+
+def get_detector_solid_angle_per_pixel(detector, run_num):
+    """
+    Get the detector polarization correction based on the detector object in psana.
+
+    :param detector: psana detector object.
+    :param run_num: The run number of the detector object
+    :return:
+    """
+    pixel_info = get_pixel_info(detector=detector, run_num=run_num)
+
+    pixel_orientation = np.zeros((pixel_info['pixel num'], 3), dtype=np.float64)
+    pixel_orientation[:, 2] = 1
+
+    solid_angle = ag.get_solid_angle(pixel_area_m=pixel_info['pixel area 1d'],
+                                     pixel_position_m=pixel_info['pixel position 1d'],
+                                     pixel_orientation=pixel_orientation,
+                                     reference_point_m=np.zeros(3, dtype=np.float64))
+
+    return solid_angle
