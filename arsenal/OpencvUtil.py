29324d6d987a3e95dd1f4fb47288f20c62721c5b
Haoyuan Li <hyli16@stanford.edu>
2018-12-04 20:18:47 -0800

    minor

--- arsenal/OpencvUtil.py
+++ arsenal/OpencvUtil.py
@@ -7,18 +7,69 @@ module mainly for myself since I am not yet familiar with the usage of opencv.
 import cv2 as opencv
 
 
-def cartesian_to_polar(source_image, center, max_radius):
+def cartesian_to_polar(cartesian_image, center, max_radius):
     """
     This function map an image over a cartesian grid to a polar grid.
     This function fill pixels outside the region with 0s.
 
-    :param source_image: The source image.
+    :param cartesian_image: The source image.
     :param center: The center of the map in the cartesian coordinate
     :param max_radius: The maximum radius to map.
     :return: The image on a polar grid.
     """
-    return opencv.linearPolar(src=source_image,
+    return opencv.linearPolar(src=cartesian_image,
                               center=center,
                               maxRadius=max_radius,
                               flags=opencv.WARP_FILL_OUTLIERS)
 
+
+def cartesian_to_polar_quick(cartesian_image, polar_image, center, max_radius):
+    """
+    This function map an image over a cartesian grid to a polar grid.
+    This function fill pixels outside the region with 0s.
+
+    :param cartesian_image: The source image.
+    :param center: The center of the map in the cartesian coordinate
+    :param max_radius: The maximum radius to map.
+    :param polar_image: The variable holding the image on the polar grid.
+    :return: The image on a polar grid.
+    """
+    return opencv.linearPolar(src=cartesian_image,
+                              dst=polar_image,
+                              center=center,
+                              maxRadius=max_radius,
+                              flags=opencv.WARP_FILL_OUTLIERS)
+
+
+def polar_to_cartesian(polar_image, center, max_radius):
+    """
+    This function map an image over a cartesian grid to a polar grid.
+    This function fill pixels outside the region with 0s.
+
+    :param polar_image: The source image.
+    :param center: The center of the map in the cartesian coordinate
+    :param max_radius: The maximum radius to map.
+    :return: The image on a polar grid.
+    """
+    return opencv.linearPolar(src=polar_image,
+                              center=center,
+                              maxRadius=max_radius,
+                              flags=[opencv.WARP_FILL_OUTLIERS, opencv.WARP_INVERSE_MAP])
+
+
+def polar_to_cartesian_quick(polar_image, cartesian_image, center, max_radius):
+    """
+    This function map an image over a cartesian grid to a polar grid.
+    This function fill pixels outside the region with 0s.
+
+    :param polar_image: The source image.
+    :param center: The center of the map in the cartesian coordinate
+    :param max_radius: The maximum radius to map.
+    :param cartesian_image: The variable hold the image on the cartesian grid.
+    :return: The image on a polar grid.
+    """
+    return opencv.linearPolar(src=polar_image,
+                              center=center,
+                              dst=cartesian_image,
+                              maxRadius=max_radius,
+                              flags=[opencv.WARP_FILL_OUTLIERS, opencv.WARP_INVERSE_MAP])
