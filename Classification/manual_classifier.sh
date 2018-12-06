788769ce1960db200434030eb71245e548a029a2
Haoyuan Li <hyli16@stanford.edu>
2018-12-05 21:23:21 -0800

    update bash

--- Classification/manual_classifier.sh
+++ Classification/manual_classifier.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+run_num=$1
+
 python /reg/neh/home5/haoyuan/Documents/my_repos/Arsenal/Classification/manual_classifier.py \
---input_folder ../output
+--input_folder ../output/run${run_num}
